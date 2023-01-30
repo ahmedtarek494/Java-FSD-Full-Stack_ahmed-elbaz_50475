b86c95bd7a8f7f7d09ec4900a5065b26dee838ca
ahmed tarek <a.tarek4.1994@gmail.com>
2023-01-30 13:17:38 +0000

    adding FileProcessing class

--- Phase 1/code/LockedMe.com/src/main/LockedMeMain.java
+++ Phase 1/code/LockedMe.com/src/main/LockedMeMain.java
@@ -1,10 +1,21 @@
 package main;
 
+import java.util.Scanner;
+
+import service.FileProcessing;
+
 public class LockedMeMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		FileProcessing file= new FileProcessing();
+		file.readAllFiles();
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Plz enter the file name which you want to create");
+		String fileName = sc.next();
+		file.createNewFile(fileName);
 	}
 
 }
---
+++ Phase 1/code/LockedMe.com/src/service/FileProcessing.java
@@ -0,0 +1,49 @@
+package service;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileProcessing {
+
+	private String folderName ="files";
+	public String[] readAllFiles() {
+		String[] ss = null;
+		try {
+			File ff = new File(folderName);
+			ss = ff.list();
+			if (ss.length == 0) {
+				System.out.println("Folder is empty, no files founded");
+			} else {
+				System.out.println("There are " + ss.length + " files founded");
+
+			}
+		} catch (Exception e) {
+			System.out.println("There's error while trying to fetch file, please try again later");
+		}
+
+		return ss;
+
+	}
+	public void createNewFile(String fileName) {
+		String filePath=folderName+"/"+fileName;
+		File ff = new File(filePath);
+		if(ff.exists()) {
+			System.out.println("File already exists");
+		}else {
+				//ff.createNewFile();
+			FileWriter fw;
+			try {
+				fw = new FileWriter(filePath);
+				fw.write("Welcome user your create file with name as "+fileName);
+				fw.flush();
+			} catch (IOException e) {
+				System.out.println("Error while creating this file in this path");
+
+			}
+		
+			
+			System.out.println("File created...");
+		}
+	}
+}
